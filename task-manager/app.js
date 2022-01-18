bde6fe786799ec61c5bcf3db35babb246ad1158b
Atharva Mohite <atharvamohite20@gmail.com>
2022-01-18 17:37:13 +0530

    updated with Dynamo db

--- task-manager/app.js
+++ task-manager/app.js
@@ -8,19 +8,85 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // Connect db
 const connectDB = require("./db/connect");
+const {
+  getData,
+  getDataById,
+  addOrUpdateData,
+  deleteData,
+} = require("./dynamo");
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Routes
-app.use("/api/v1/tasks", tasks);
+app.get("/", (req, res) => {
+  res.json({
+    data: `server is running!!!!`,
+  });
+});
+
+app.get("/data", async (req, res) => {
+  try {
+    const data = await getData();
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.get("/data/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const data = await getDataById(id);
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.post("/data", async (req, res) => {
+  const data = req.body;
+  console.log("body: ", data);
+  try {
+    const newData = await addOrUpdateData(data);
+    res.json(newData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.put("/data/:id", async (req, res) => {
+  const data = req.body;
+  const { id } = req.params;
+  data.id = id;
+  try {
+    const newData = await addOrUpdateData(data);
+    res.json(newData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.delete("/data/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    res.json(await deleteData(id));
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    // await connectDB(process.env.MONGO_URI);
     app.listen(PORT, () => {
       console.log(`Server listening at port ${PORT}...`);
     });
---
+++ task-manager/dynamo.js
@@ -0,0 +1,70 @@
+const AWS = require("aws-sdk");
+require("dotenv").config();
+
+AWS.config.update({
+  region: process.env.AWS_DEFAULT_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  //   endpoint: "http://localhost:8000",
+});
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = "testing";
+
+const getData = async () => {
+  const params = {
+    TableName: TABLE_NAME,
+  };
+  try {
+    console.log("In try ");
+    const data = await dynamoClient.scan(params).promise();
+    console.log("Return data: ", data);
+    return data;
+  } catch (e) {
+    console.log("In catch ");
+    console.log("the error is: ", e);
+  }
+};
+// getData();
+
+const DATA = {
+  testingId: "1",
+  name: "test1",
+};
+
+const addOrUpdateData = async (character) => {
+  const params = {
+    TableName: TABLE_NAME,
+    Item: character,
+  };
+  return await dynamoClient.put(params).promise();
+};
+// addOrUpdateData(DATA);
+
+const getDataById = async (id) => {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      testingId: id,
+    },
+  };
+  return await dynamoClient.get(params).promise();
+};
+
+const deleteData = async (id) => {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      testingId: id,
+    },
+  };
+  return await dynamoClient.delete(params).promise();
+};
+
+module.exports = {
+  dynamoClient,
+  getData,
+  getDataById,
+  addOrUpdateData,
+  deleteData,
+};
